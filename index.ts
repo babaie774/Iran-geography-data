bae1ca8f230298ab7cd0f254c5f1777de4008e5d
Alireza Babaei <babaie774@gmail.com>
2025-09-22 01:24:02 +0330

    fix: geo-data-file

---
+++ index.ts
@@ -0,0 +1,31 @@
+// Example data for geo API endpoints
+export const filterProvinceExample = {
+  page: 1,
+  limit: 10,
+  name: 'تهران',
+  id: 1,
+  parentId: 1,
+};
+
+export const filterCityExample = {
+  page: 1,
+  limit: 10,
+  name: 'تهران',
+  id: 1,
+  parentId: '1',
+};
+
+export const filterNeighborhoodExample = {
+  page: 1,
+  limit: 10,
+  name: 'ونک',
+  slug: 'vanak',
+  parentId: 1,
+  cityId: 1,
+};
+
+export const searchLocationsExample = {
+  page: 1,
+  limit: 10,
+  q: 'تهران',
+};
